5525d0ac3818c4ad36b2addb71076f15eb500d9b
Brooke Bryan <brooke@bajb.net>
2016-06-16 12:23:05 +0100

    Webhooks helper

---
+++ src/Webhooks.php
@@ -0,0 +1,40 @@
+<?php
+namespace Fortifi\Webhooks;
+
+use Fortifi\Webhooks\Events\AdvertiserWHE;
+use Fortifi\Webhooks\Events\OrderWHE;
+use Fortifi\Webhooks\Events\PurchaseWHE;
+use Fortifi\Webhooks\Payloads\Advertiser\AdvertiserCreatedWHP;
+use Fortifi\Webhooks\Payloads\Advertiser\Campaign\AdvertiserCampaignCreatedWHP;
+use Fortifi\Webhooks\Payloads\Order\OrderCreatedWHP;
+use Fortifi\Webhooks\Payloads\Order\OrderStateChangedWHP;
+use Fortifi\Webhooks\Payloads\Purchase\PurchaseCreatedWHP;
+use Fortifi\Webhooks\Payloads\Purchase\PurchaseStateChangedWHP;
+use Packaged\Helpers\Objects;
+
+class Webhooks
+{
+  public static function getPayload($json)
+  {
+    $decoded = json_decode($json);
+    $events = static::all();
+    $event = Objects::property($decoded, 'event');
+    if(isset($events[$event]))
+    {
+      return call_user_func([$events[$event], 'hydrateFromJson'], $json);
+    }
+    return null;
+  }
+
+  public static function all()
+  {
+    return [
+      AdvertiserWHE::CREATED          => AdvertiserCreatedWHP::class,
+      AdvertiserWHE::CAMPAIGN_CREATED => AdvertiserCampaignCreatedWHP::class,
+      OrderWHE::CREATED               => OrderCreatedWHP::class,
+      OrderWHE::STATE_CHANGE          => OrderStateChangedWHP::class,
+      PurchaseWHE::CREATED            => PurchaseCreatedWHP::class,
+      PurchaseWHE::STATE_CHANGE       => PurchaseStateChangedWHP::class,
+    ];
+  }
+}
