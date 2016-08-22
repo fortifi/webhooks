801cd5356c5b64c2583ec74c696873a6bfe8d3ff
Brooke Bryan <brooke@bajb.net>
2016-08-22 15:53:57 +0100

    Link new WHE/WHPs

--- src/Webhooks.php
+++ src/Webhooks.php
@@ -2,12 +2,19 @@
 namespace Fortifi\Webhooks;
 
 use Fortifi\Webhooks\Events\AdvertiserWHE;
+use Fortifi\Webhooks\Events\CustomerWHE;
+use Fortifi\Webhooks\Events\InvoiceWHE;
 use Fortifi\Webhooks\Events\OrderWHE;
+use Fortifi\Webhooks\Events\PaymentAccountWHE;
 use Fortifi\Webhooks\Events\PurchaseWHE;
 use Fortifi\Webhooks\Payloads\Advertiser\AdvertiserCreatedWHP;
 use Fortifi\Webhooks\Payloads\Advertiser\Campaign\AdvertiserCampaignCreatedWHP;
+use Fortifi\Webhooks\Payloads\Customer\CustomerCreatedWHP;
+use Fortifi\Webhooks\Payloads\Invoice\InvoiceWHP;
 use Fortifi\Webhooks\Payloads\Order\OrderCreatedWHP;
 use Fortifi\Webhooks\Payloads\Order\OrderStateChangedWHP;
+use Fortifi\Webhooks\Payloads\PaymentAccount\PaymentAccountCreatedWHP;
+use Fortifi\Webhooks\Payloads\PaymentAccount\PaymentAccountUpdatedWHP;
 use Fortifi\Webhooks\Payloads\Purchase\PurchaseCreatedWHP;
 use Fortifi\Webhooks\Payloads\Purchase\PurchaseStateChangedWHP;
 use Packaged\Helpers\Objects;
@@ -35,6 +42,10 @@ class Webhooks
       OrderWHE::STATE_CHANGE          => OrderStateChangedWHP::class,
       PurchaseWHE::CREATED            => PurchaseCreatedWHP::class,
       PurchaseWHE::STATE_CHANGE       => PurchaseStateChangedWHP::class,
+      PaymentAccountWHE::CREATED      => PaymentAccountCreatedWHP::class,
+      PaymentAccountWHE::UPDATED      => PaymentAccountUpdatedWHP::class,
+      CustomerWHE::CREATED            => CustomerCreatedWHP::class,
+      InvoiceWHE::CREATED             => InvoiceWHP::class,
     ];
   }
 }
