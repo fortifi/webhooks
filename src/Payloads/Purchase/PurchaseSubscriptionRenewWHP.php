847cbaaa90d5dbe40ac92c74c53278fc0ed6bdb2
James Eagle <james@jdi.io>
2017-01-10 15:40:09 +0000

    Ref T2331 - New payload properties

--- src/Payloads/Purchase/PurchaseSubscriptionRenewWHP.php
+++ src/Payloads/Purchase/PurchaseSubscriptionRenewWHP.php
@@ -4,4 +4,7 @@ namespace Fortifi\Webhooks\Payloads\Purchase;
 class PurchaseSubscriptionRenewWHP extends PurchaseWHP
 {
   public $subscriptionFid;
+  public $nextRenewalDate;
+  public $nextRenewalAmount;
+  public $renewalsCount;
 }
