585c82d44fe7126e076e079f20542425408fd78e
James Eagle <james@jdi.io>
2017-01-10 11:21:32 +0000

    Ref T2313 - New payload properties

--- src/Payloads/Purchase/PurchasePriceChangedWHP.php
+++ src/Payloads/Purchase/PurchasePriceChangedWHP.php
@@ -4,5 +4,12 @@ namespace Fortifi\Webhooks\Payloads\Purchase;
 class PurchasePriceChangedWHP extends PurchaseWHP
 {
   public $oldPriceFid;
+  public $oldPriceName;
+  public $oldPriceAmount;
+  public $oldPriceCurrency;
+
   public $newPriceFid;
+  public $newPriceName;
+  public $newPriceAmount;
+  public $newPriceCurrency;
 }
