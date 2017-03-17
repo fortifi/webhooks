a30659575e2b5d8b42cc7fccec65b37428c32815
James Eagle <james.eagle@fortifi.it>
2017-03-17 10:40:23 +0000

    Ref T2523 - new webhook

---
+++ src/Payloads/Transaction/TransactionFailedWHP.php
@@ -0,0 +1,17 @@
+<?php
+namespace Fortifi\Webhooks\Payloads\Transaction;
+
+use Fortifi\Webhooks\Payloads\Foundation\DataNodeWHP;
+
+class TransactionFailedWHP extends DataNodeWHP
+{
+  public $subscriptionFid;
+  public $subscriptionName;
+  public $renewalAttemptNumber;
+  public $renewalAmount;
+  public $renewalAmountUsd;
+  public $exchangeRate;
+  public $transactionDate;
+  public $ipnErrorMessage;
+  public $customerFID;
+}
