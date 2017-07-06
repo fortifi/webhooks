5f4ce5fec918bba54b18b1b511239938679e738d
James Eagle <Jleagle@users.noreply.github.com>
2017-07-06 12:06:43 +0100

    T2810 - Webhook on Employee Logged In / Employee Logged Out (#10)
    * Ref T2810 - New employee auth webhook
    
    * Ref T2810 - rename property
    
    * Ref T2810 - Remove property

---
+++ src/Payloads/Employee/AbstractEmployeeWHP.php
@@ -0,0 +1,16 @@
+<?php
+namespace Fortifi\Webhooks\Payloads\Employee;
+
+use Fortifi\Webhooks\Payloads\Foundation\DataNodeWHP;
+
+abstract class AbstractEmployeeWHP extends DataNodeWHP
+{
+  public $position;
+  public $title;
+  public $firstName;
+  public $middleNames;
+  public $lastName;
+  public $emailFid;
+  public $isDisabled;
+  public $permissions;
+}
