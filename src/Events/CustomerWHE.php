1487d8033c458d7e5b1d503621343d627982be84
Tom Kay <oridan82@gmail.com>
2018-05-10 16:31:15 +0100

    SAR webhook (#19)
    * add subject access request webhook
    
    * add gitignore

--- src/Events/CustomerWHE.php
+++ src/Events/CustomerWHE.php
@@ -4,4 +4,5 @@ namespace Fortifi\Webhooks\Events;
 final class CustomerWHE
 {
   const CREATED = 'customer.created';
+  const SUBJECT_ACCESS_REQUEST = 'customer.sar';
 }
---
+++ src/Payloads/Customer/SubjectAccessRequestWHP.php
@@ -0,0 +1,9 @@
+<?php
+namespace Fortifi\Webhooks\Payloads\Customer;
+
+use Fortifi\Webhooks\Payloads\FortifiWebhookPayload;
+
+class SubjectAccessRequestWHP extends FortifiWebhookPayload
+{
+  public $url;
+}
