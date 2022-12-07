a9cc29eba599d1569fdd51084d669b870fbe607b
Jleagle <jimeagle@gmail.com>
2022-12-07 15:54:18 +0000

    Fix sar payload

--- src/Payloads/Customer/SubjectAccessRequestWHP.php
+++ src/Payloads/Customer/SubjectAccessRequestWHP.php
@@ -3,7 +3,7 @@ namespace Fortifi\Webhooks\Payloads\Customer;
 
 use Fortifi\Webhooks\Payloads\FortifiWebhookPayload;
 
-class SubjectAccessRequestWHP extends FortifiWebhookPayload
+class SubjectAccessRequestWHP extends CustomerWHP
 {
   public $url;
 }
