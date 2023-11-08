fc2262a8845c9ba7ade578d2686154169845f93b
Jleagle <jimeagle@gmail.com>
2023-11-08 16:17:11 +0000

    DEV-512: Add webhook when the default email changes. (#32)
    * New webhook
    
    * Add to registry
    
    * Format

--- src/Events/CustomerWHE.php
+++ src/Events/CustomerWHE.php
@@ -6,4 +6,5 @@ final class CustomerWHE
   const CREATED = 'customer.created';
   const SUBJECT_ACCESS_REQUEST = 'customer.sar';
   const EMAIL_UNSUBSCRIBED = 'customer.email.unsubscribed';
+  const DEFAULT_EMAIL_UPDATED = 'customer.default-email.updated';
 }
