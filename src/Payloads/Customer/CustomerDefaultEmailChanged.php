401003ca6e3b6a6ead8295357f2bd7bb92f1cfc3
Jleagle <jimeagle@gmail.com>
2023-11-08 16:12:08 +0000

    New webhook

---
+++ src/Payloads/Customer/CustomerDefaultEmailChanged.php
@@ -0,0 +1,9 @@
+<?php
+namespace Fortifi\Webhooks\Payloads\Customer;
+
+use Fortifi\Webhooks\Payloads\FortifiWebhookPayload;
+
+class CustomerDefaultEmailChanged extends CustomerWHP
+{
+  public $emailFid;
+}
